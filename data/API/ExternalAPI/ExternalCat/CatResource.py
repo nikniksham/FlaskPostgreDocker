0a53ec971ea4632376105734e22af580ac126c3e
Niki <57631867+nikniksham@users.noreply.github.com>
2022-01-29 19:46:58 +0300

    tmp

--- data/API/ExternalAPI/ExternalCat/CatResource.py
+++ data/API/ExternalAPI/ExternalCat/CatResource.py
@@ -1,6 +1,4 @@
-import datetime
-import json
-from flask import jsonify, request
+from flask import jsonify
 from flask_restful import Resource
 from SessionManager import Session
 from data.API.ExternalAPI.ExternalCat.cat_parser import parser_cat
@@ -15,132 +13,61 @@ def find_by_id(id, session):
     return cat, session
 
 
-# class NewspageResourceUsual(Resource):
-#     def get(self, newspage_id):
-#         session = db_session.create_session()
-#         newspage, session = find_by_id(newspage_id, session)
-#         session.close()
-#         news_dict = newspage.to_dict(only=('id', 'heading', 'text', 'link', 'image', 'tags', 'created_date'))
-#         news_dict["mini_text"] = mini_text(newspage.text)
-#         news_dict["text_render"] = text_transform(newspage.text, newspage.image.split("//"), path)
-#         return jsonify(news_dict)
-#
-#
-# class NewspageResourceLink(Resource):
-#     def get(self, link):
-#         session = db_session.create_session()
-#         newspage = session.query(Newspage).filter(Newspage.link == link).first()
-#         session.close()
-#         if newspage:
-#             news_dict = newspage.to_dict(only=('id', 'heading', 'text', 'link', 'image', 'tags', 'created_date'))
-#             news_dict["mini_text"] = mini_text(newspage.text)
-#             news_dict["text_render"] = text_transform(newspage.text, newspage.image.split("//"), path)
-#             return jsonify(news_dict)
-#         raise_error("Новость не найдена")
-#
-#
-# class NewspageListRecourseId(Resource):
-#     def get(self, start_id, end_id):
-#         session = db_session.create_session()
-#         newspages = session.query(Newspage).order_by(Newspage.created_date)[::-1]
-#         session.close()
-#         if start_id > len(newspages):
-#             return jsonify([])
-#         if end_id > len(newspages):
-#             end_id = len(newspages)
-#         newspages, news_list = newspages[start_id:end_id], []
-#         for item in newspages:
-#             news_dict = item.to_dict(only=('id', 'heading', 'text', 'link', 'image', 'tags', 'created_date'))
-#             news_dict["mini_text"] = mini_text(item.text)
-#             news_dict["text_render"] = text_transform(item.text, item.image.split("//"), path)
-#             news_list.append(news_dict)
-#         return jsonify(news_list)
+class CatResourceUsual(Resource):
+    def get(self, cat_id):
+        session = Session()
+        cat, session = find_by_id(cat_id, session)
+        cat_dict = cat.to_dict()
+        session.close()
+        return jsonify(cat_dict)
 
 
-# class NewspageListRecourseTags(Resource):
-#     def post(self, start_id, end_id):
-#         """
-#         session = db_session.create_session()
-#         pages, newspages = session.query(Newspage).order_by(Newspage.created_date)[::-1], []
-#         session.close()
-#         if text is None:
-#             text = ""
-#         find_text = text.replace("<", "").replace(">", "").replace("/", "").lower().rstrip()
-#         for news in pages:
-#             tags = news.tags.lower() if news.tags else ""
-#             text = news.text.lower() if news.text else ""
-#             heading = news.heading.lower() if news.heading else ""
-#             if find_text in tags or find_text in text or find_text in heading:
-#                 newspages.append(news)
-#         if start_id > len(newspages):
-#             return jsonify([])
-#         if end_id > len(newspages):
-#             end_id = len(newspages)
-#         newspages, news_list = newspages[start_id:end_id], []
-#         for item in newspages:
-#             news_dict = item.to_dict(only=('id', 'heading', 'text', 'link', 'image', 'tags', 'created_date'))
-#             news_dict["mini_text"] = mini_text(item.text)
-#             news_dict["text_render"] = text_transform(item.text, item.image.split("//"), path)
-#             news_list.append(news_dict)
-#         """
-#         dick = {}
-#         params = json.loads(request.form['canvas_data'])
-#         text = params["text"]
-#         write_log(text)
-#         session = db_session.create_session()
-#         news_pages = session.query(Newspage).order_by(Newspage.created_date).all()
-#         if text:
-#             for news_page in news_pages:
-#                 for word in text.split():
-#                     if news_page.tags and word.lower() in news_page.tags.lower():
-#                         if news_page in dick:
-#                             dick[news_page] += 10
-#                         else:
-#                             dick[news_page] = 10
-#                     if news_page.heading and word.lower() in news_page.heading.lower():
-#                         if news_page in dick:
-#                             dick[news_page] += 4
-#                         else:
-#                             dick[news_page] = 4
-#                     if news_page.text and word.lower() in news_page.text.lower():
-#                         if news_page in dick:
-#                             dick[news_page] += 1
-#                         else:
-#                             dick[news_page] = 1
-#             news_list = []
-#             for compare in list(set(dick.values())):
-#                 a = []  # тут у ники случился инсульт жопы
-#                 for key in dick.keys():
-#                     if dick[key] == compare:
-#                         a.append(key)
-#                 a.sort(key=lambda x: x.created_date, reverse=True)
-#                 news_list.extend(a)
-#         else:
-#             news_list = news_pages
-#         if start_id > len(news_list):
-#             return jsonify([])
-#         if end_id > len(news_list):
-#             end_id = len(news_list)
-#         newspages, news_list = news_list[start_id:end_id], []
-#         write_log(f"I am here")
-#         for item in newspages:
-#             news_dict = item.to_dict(only=('id', 'heading', 'text', 'link', 'image', 'tags', 'created_date'))
-#             news_dict["mini_text"] = mini_text(item.text)
-#             news_dict["text_render"] = text_transform(item.text, item.image.split("//"), path)
-#             write_log(f"Add news {news_dict['heading']}")
-#             news_list.append(news_dict)
-#         return jsonify(news_list)
-#
-#
-# class NewspageListRecourse(Resource):
-#     def get(self):
-#         session = db_session.create_session()
-#         newspages, news_list = session.query(Newspage).order_by(Newspage.created_date)[::-1], []
-#         for item in newspages:
-#             news_dict = item.to_dict(only=('id', 'heading', 'text', 'link', 'image', 'tags', 'created_date'))
-#             news_dict["mini_text"] = mini_text(item.text)
-#             news_dict["text_render"] = text_transform(item.text, item.image.split("//"), path)
-#             news_list.append(news_dict)
-#         session.close()
-#         return jsonify(news_list)
+class CatListResource(Resource):
+    def get(self, page):
+        session = Session()
+        cats = session.query(Cat).all()
+        cats_list, need = [], [6 * page, 6 * (page + 1) - 1]
+        if cats:
+            for cat_id, cat in enumerate(cats):
+                if need[0] <= cat_id <= need[1]:
+                    cats_list.append(cat.to_dict())
+                elif cat_id > need[1]:
+                    break
+            session.close()
+            return jsonify(cats_list)
+        session.close()
+        raise_error("Котики для этой страницы не найдены :(")
 
+
+class CatRelevantListRecourse(Resource):
+    def put(self, count):
+        session = Session()
+        cats = session.query(Cat).all()
+        cat_list = []
+        arr_with_mass = []
+        if cats:
+            args = parser_cat.parse_args()
+            for cat in cats:
+                mass = 0
+                if args['species'] and cat.species == args['species']:
+                    mass += 2
+                if args['gender'] and cat.gender == args['gender']:
+                    mass += 2
+                if args['age'] and cat.age == args['age']:
+                    mass += 2
+                if args['text']:
+                    for w in args['text']:
+                        if w in cat.name:
+                            mass += 1
+                            break
+                    for w in args['text']:
+                        if w in cat.description:
+                            mass += 1
+                            break
+                arr_with_mass.append([cat, mass])
+            arr_with_mass.sort(key=lambda x: -x[1])
+            for cat in arr_with_mass[:min(count, len(arr_with_mass))]:
+                cat_list.append(cat[0].to_dict())
+            session.close()
+            return cat_list
+        raise_error("А всё, коты то кончились", session)
--- run.py
+++ run.py
@@ -1,27 +1,204 @@
-import datetime
+import os
+import random
+import shutil
+import threading
+from PIL import Image
 from flask_restful import Api
-from flask import Flask, render_template, url_for, request, send_from_directory, flash
+from flask import Flask, render_template, request
 from flask_login import LoginManager, login_required, logout_user, current_user, login_user
-from werkzeug.utils import redirect
+from werkzeug.utils import redirect, secure_filename
 import FlaskConfig
 from SessionManager import Session
 from data.forms import LoginForm
 from data.models.user import User
 from flask_sqlalchemy import SQLAlchemy
+from data.API.ExternalAPI.ExternalCat.CatResource import CatResourceUsual, CatListResource, CatRelevantListRecourse
 
 db = SQLAlchemy()
 
+admin_images = {}
+let = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
 ALLOWED_EXTENSIONS = ['pdf', 'png', 'jpg', 'jpeg']
 application = Flask(__name__)
 application.config.from_object(FlaskConfig)
 
-# db_session.global_init("db/db_for_proj.sql")
 login_manager = LoginManager()
 login_manager.init_app(application)
 
 api = Api()
 api.add_resource(application)
-# api.add_resource()
+api.add_resource(CatResourceUsual, "/api/cat/<int: cat_id>")
+api.add_resource(CatListResource, "/api/cat_list/<int: page>")
+api.add_resource(CatRelevantListRecourse, "/api/cat_relevant/<int: count>")
+
+
+def get_path(end=""):
+    return f"tmp/{current_user.email}{end}"
+
+
+def save_image_multithreading(filename, file, feedback=False, favic=False):
+    size = (720, 480) if feedback else (1920, 1080)
+    path = "/".join(filename.split("/")[:-1])
+    if not os.path.exists(path):
+        os.makedirs(path)
+    file.save(filename)
+    with Image.open(filename) as image:
+        if image.size[0] > size[0] or image.size[1] > size[1]:
+            image.thumbnail(size)
+        split_name = filename.split('.')
+        path, format = '.'.join(split_name[:-1]), split_name[-1]
+        if not favic:
+            image.save(filename)
+        else:
+            image.save(filename, format="ICO", sizes=[(64, 64)])
+
+
+def save_image(filename, file, feedback=False, favic=False):
+    t1 = threading.Thread(target=save_image_multithreading, args=(os.path.join(application.config["UPLOAD_FOLDER"], filename), file, feedback, favic))
+    t1.start()
+    t1.join()
+
+
+def clear_folder(folder_name, path=application.config['UPLOAD_FOLDER']):
+    delete_folder(folder_name, path=path)
+    os.makedirs(path+folder_name)
+
+
+def clear_old_files(key, path):
+    admin_images[key] = []
+    delete_folder(path)
+
+
+def copy_image(path, old_name, new_name):
+    if os.path.exists(path+old_name):
+        if os.path.exists(path+new_name):
+            delete_img(path+new_name)
+        shutil.copy(f"{path}{old_name}", f"{path}{new_name}")
+        return True
+    return False
+
+
+def copy_files(old_folder, new_folder, filenames):
+    path, new_filenames = application.config['UPLOAD_FOLDER'], []
+    clear_folder(new_folder)
+    if os.path.exists(path+old_folder):
+        for filename in filenames:
+            if filename != "" and os.path.exists(path + filename):
+                new_filenames.append(f"{new_folder}/{filename.split('/')[-1]}")
+                shutil.copy(f"{path}{old_folder}/{filename.split('/')[-1]}", f"{path}{new_folder}/{filename.split('/')[-1]}")
+    return new_filenames
+
+
+def transport_images(filenames, new_folder, path=application.config['UPLOAD_FOLDER']):
+    new_filenames = []
+    clear_folder(new_folder)
+    for filename in filenames:
+        if os.path.exists(path+filename):
+            os.replace(path+filename, f'{path}{new_folder}/{filename.split("/")[-1]}')
+            new_filenames.append(f'{new_folder}/{filename.split("/")[-1]}')
+    # delete_folder(old_folder)
+    return new_filenames
+
+
+def get_files_from(folder, path=application.config["UPLOAD_FOLDER"]):
+    files = []
+    if os.path.exists(path+folder):
+        files = os.listdir(path+folder)
+    files = [folder+"/"+f for f in files]
+    return files
+
+
+def delete_everything_except(folder_name, filenames, path=application.config['UPLOAD_FOLDER']):
+    if os.path.exists(path+folder_name):
+        for filename in os.listdir(path + folder_name):
+            if f"{folder_name}/{filename}" not in filenames and os.path.exists(f"{path}{folder_name}/{filename}"):
+                os.remove(f"{path}{folder_name}/{filename}")
+
+
+def delete_folder(folder_name, path=application.config['UPLOAD_FOLDER']):
+    if os.path.exists(path+folder_name):
+        for filename in os.listdir(path + folder_name):
+            if os.path.isdir(f"{path}{folder_name}/{filename}"):
+                delete_folder(f"{folder_name}/{filename}")
+            else:
+                while True:
+                    try:
+                        os.remove(f"{path}{folder_name}/{filename}")
+                        break
+                    except:
+                        pass
+        os.rmdir(path+folder_name)
+
+
+def delete_img(filename):
+    if filename not in ["", "standard.png"] and os.path.exists(f"{application.config['UPLOAD_FOLDER']}{filename}"):
+        os.remove(f"{application.config['UPLOAD_FOLDER']}{filename}")
+
+
+def save_images(files, path, email, r_img=False, gif=True, logo=False, feedback=False, max_image=None):  # teleport
+    old_files, new_files, img_list, s = [], [], list(files), 0
+    dict = admin_images
+    if email in dict:
+        old_files = dict[email]
+    for index, elem in enumerate(list(files) if max_image and len(files) == max_image else list(files)[:-1]):
+        if max_image and index > max_image:
+            break
+        ind, file = "".join(list(filter(lambda x: x.isdigit(), list(elem)))), files[elem]
+        if not ind.isdigit() and not logo:
+            continue
+        ind = int(ind) - 1 if ind.isdigit() else 0
+        if files[elem].filename != "" and allowed_file(file.filename, feedback):
+            gif_i = True if file.filename.split(".")[-1] == "gif" else False
+            png = True if file.filename.split(".")[-1] == "png" else False
+            if logo:
+                if "icon" in img_list[ind]:
+                    filename = secure_filename(create_new_image_name())
+                    save_image(f"{path}/" + filename, file)
+                    new_files.append(filename)
+                    break
+            else:
+                if gif_i:
+                    filename = secure_filename(create_new_image_name())
+                else:
+                    filename = secure_filename(create_new_image_name())
+                save_image(f"{path}/"+filename, file, feedback)
+                new_files.append(filename)
+        elif ind < len(old_files):
+            new_files.append(old_files[ind])
+    for file in old_files:
+        if file not in new_files:
+            delete_img(file)
+    if len(new_files) == 0 and r_img:
+        r_name = f"{create_random_name(50)}.png"
+        img = Image.open(f"{application.config['UPLOAD_FOLDER']}check.png")
+        if not os.path.exists(f"{application.config['UPLOAD_FOLDER']}{path}"):
+            os.makedirs(f"{application.config['UPLOAD_FOLDER']}{path}")
+        img.save(f"{application.config['UPLOAD_FOLDER']}{path}/{r_name}")
+        new_files.append(r_name)
+    dict[email] = new_files
+    new_files = [f"{path}/"+_ for _ in new_files]
+    delete_everything_except(path, new_files)
+    return new_files
+
+
+def create_new_image_name():
+    filelist, format = os.listdir(application.config['UPLOAD_FOLDER']), ".png"
+    filename = create_random_name(50) + format
+    while filename in filelist:
+        filename = create_random_name(50) + format
+    return filename
+
+
+def create_random_name(name_len):
+    return ''.join([random.choice(let) for i in range(name_len)])
+
+
+def allowed_file(filename, feedback=False):
+    ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif', 'mp4']
+    ALLOWED_EXTENSIONS_FEEDBACK = ['png', 'jpg', 'jpeg']
+    if feedback:
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS_FEEDBACK
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @login_manager.user_loader
