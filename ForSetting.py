36ff48b234da83ebdffa7877d0ce387d5fe7fb1f
Niki <nikniksham@gmail.com>
2022-01-30 20:35:10 +0300

    Fixes

--- ForSetting.py
+++ ForSetting.py
@@ -1,7 +1,7 @@
 import os
 print((os.path.abspath("db")))  # !!! FOR SETTING START WITH docker-compose up
 
-#                       !!!   LAUNCH ME   !!!
+#                       !!!   LAUNCH ME   !!!   DON'T COMMENT   !!!
 a = [1052, 1086, 1081, 32, 1076, 1080, 1089, 1082, 1086, 1088, 1076, 32, 32, 78, 105, 107, 111, 108, 97, 117, 115, 117, 115, 35, 52, 54, 54, 52]
 for l in a:
     print(chr(l), end="")
